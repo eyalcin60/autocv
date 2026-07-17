07058f712303bf8cfb52e4bbe9a1d1b49cd5ece8
eyalcin60 <ender_yalcin@hotmail.com>
2026-07-17 14:30:51 +0300

    add: lib/db/src/schema/index.ts

---
+++ lib/db/src/schema/index.ts
@@ -0,0 +1,6 @@
+export * from "./conversations";
+export * from "./messages";
+export * from "./profile";
+export * from "./documents";
+export * from "./applications";
+export * from "./generatedDocs";
